3c8ae3c0cf70b162c2fb6020f4118b830fe5ac64
simonojok19 <simonojok19@gmail.com>
2020-03-29 08:17:38 +0300

    created home.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:ch4_starter_exercise/pages/home.dart';
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Starter Template',
+      theme: ThemeData (
+        primarySwatch: Colors.blue
+      ),
+      home: Home(),
+    );
+  }
+}
